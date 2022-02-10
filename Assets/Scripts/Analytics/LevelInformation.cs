ac823820b003b7bb97daf53544a5e97502753127
Vasiliy <v.nichnikov@gmail.com>
2022-02-10 23:18:30 +0300

    Добавлены плагины для аналитики
    Написан простой скрипт, отвечающий за работу с Яндекс.Аналитика

---
+++ Assets/Scripts/Analytics/LevelInformation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Analytics
+{
+    public class LevelInformation : MonoBehaviour
+    {
+        public int LevelNumber => SceneManager.GetActiveScene().buildIndex + 1;
+        public string LevelName => SceneManager.GetActiveScene().name;
+        public TypesOfLevelDifficulties LevelDifficulties => _levelDifficulties;
+        
+        
+        [SerializeField, Header("Сложность уровня")]
+        private TypesOfLevelDifficulties _levelDifficulties;
+        
+    }
+}
